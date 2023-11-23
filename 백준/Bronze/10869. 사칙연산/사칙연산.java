d35e3a4f120bfa6387ac45393e63acacdf7f412f
Jonghyuk Lim <75292389+imLim0813@users.noreply.github.com>
2023-11-23 09:43:40 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String[] array = scanner.nextLine().split("\\s");
+        System.out.println(Integer.parseInt(array[0]) + Integer.parseInt(array[1]));
+        System.out.println(Integer.parseInt(array[0]) - Integer.parseInt(array[1]));
+        System.out.println(Integer.parseInt(array[0]) * Integer.parseInt(array[1]));
+        System.out.println(Integer.parseInt(array[0]) / Integer.parseInt(array[1]));
+        System.out.println(Integer.parseInt(array[0]) % Integer.parseInt(array[1]));
+    }
+}
