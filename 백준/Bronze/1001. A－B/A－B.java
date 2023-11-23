e14a303f831c3f7cfb7a12f0142a96cc25e91321
Jonghyuk Lim <75292389+imLim0813@users.noreply.github.com>
2023-11-23 09:22:24 +0900

    [Bronze V] Title: A-B, Time: 112 ms, Memory: 12952 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        String[] array = tmp.split("\\s");
+        System.out.println(Integer.parseInt(array[0]) - Integer.parseInt(array[1]));
+    }
+}
