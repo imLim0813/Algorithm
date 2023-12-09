37a7c676bbda06fbe5e0916752f86f804af6a29c
Jonghyuk Lim <75292389+imLim0813@users.noreply.github.com>
2023-12-09 10:37:47 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1088 ms, Memory: 299036 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++)
+        {
+            String s = br.readLine();
+            int a = Integer.parseInt(s.split(" ")[0]);
+            int b = Integer.parseInt(s.split(" ")[1]);
+            bw.write(a + b + "\n");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
